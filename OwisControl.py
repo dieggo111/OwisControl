30bf0ab982d49e73c04c99d2399aa1b2b4b9c5ed
dieggo111 <marius.metzler@kit.edu>
2016-12-14 17:20:22 +0100

    added getErr and ref method

--- OwisControl.py
+++ OwisControl.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import serial
 
 class owis:
@@ -13,26 +14,39 @@ class owis:
     
     def init(self):
 
-# Folgende Parameter müssen beim Initialisieren übermittelt werden: Motortyp,
-# Endschalter-Maske, Endschalter-Polarität, Achsenparameter/Regelparameter, 
-# Strombereich der Motorendstufe. Erst dann kann gefahren werden.
+    # Folgende Parameter muessen beim Initialisieren uebermittelt werden: Motortyp,
+    # Endschalter-Maske, Endschalter-Polaritaet, Achsenparameter/Regelparameter, 
+    # Strombereich der Motorendstufe. Erst dann kann gefahren werden.
 
         print "Initializing..."
         
-#        self.ser.write("?ERR\r\n")
-#        err = self.ser.readline()
-#        if err is not "0":
-#            print "Unsolved error(s) in memory:\n"
-#            for el in err:
-#                print el
-#        self.ser.write("ERRCLEAR\r\n") 
+#        # Motortyp waehlen (3 = Schrittmotor Closed-Loop)
+#        self.ser.write("MOTYPE3\r\n")
+#        # Strombereich für Achse 1/2/3 waehlen (0 = niedrig)        
+#        for i in range(1, 4):        
+#            self.ser.write("AMPSHNT" + str(i) + "=0\r\n")  
+        
+
+        # Achse 1/2/3 initialisieren mit gespeicherten Standardparametern        
+        for i in range(1, 4):        
+            self.ser.write("INIT" + str(i) + "\r\n")
+        for i in range(1, 4):        
+            print "INIT" + str(i) + "\r\n"
+                    
+    
+    def ref(self):
+
+        # Referenzmaske fuer Achse 1/2/3 waehlen      
+        for i in range(1, 4):        
+            self.ser.write("RMK" + str(i) + "=0001\r\n")
+        # Referenzschalterpolaritaet fuer Achse !/2/3 waehlen
+        for i in range(1, 4):        
+            self.ser.write("RPL" + str(i) + "=0001\r\n")
+         
+        return True
 
         
-        # Motortyp wählen (3 = Schrittmotor Closed-Loop)
-        self.ser.write("MOTYPE3\r\n")
-        # Strombereich für Achse 1/2/3 wählen (0 = niedrig)        
-        for i in range(1, 3):        
-            self.ser.write("AMPSHNT" + str(i) + "=0\r\n")        
+  
 
     def check(self):
 
@@ -41,6 +55,9 @@ class owis:
             print(self.ser.name + ' is open...')
         else:
             print "Could not find device :("
+
+        return True
+
 
     def test(self):
 
@@ -53,7 +70,28 @@ class owis:
                 answer = self.ser.readline()
                 print answer
 
+        return True
 
+
+    def getErr(self):
+                
+        self.ser.write("?ERR\r\n")
+        err = self.ser.readline()
+        if err is not "0":
+            print "Unsolved error(s) in memory\n"
+            for el in err:
+                print el + "\n"
+        else:
+            print "No errors to report\n"
+        cmd = raw_input("Clear memory (y/n)?:\n")
+        while (cmd is not "y") or (cmd is not "n"):
+            cmd = raw_input("Repeat input: Clear memory (y/n)?:\n")
+            if cmd == "y":
+                self.ser.write("ERRCLEAR\r\n") 
+            elif cmd == "n":
+                pass
+
+        return True
 
 # main loop
 if __name__=='__main__':
