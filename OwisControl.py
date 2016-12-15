e0c6aef2945984918f7d45e8dd7c1a190345b201
dieggo111 <marius.metzler@kit.edu>
2016-12-15 15:55:45 +0100

    Added moveAbs and checkPos method ... it s moving!

--- OwisControl.py
+++ OwisControl.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import serial
+import time
 
 class owis:
 
@@ -8,67 +9,73 @@ class owis:
         port = "COM5"
         baud = 9600
         bytesize = 8                    
-    
-        self.ser = serial.Serial(port, baud, bytesize, timeout=0.5)
+            
+        self.ser = serial.Serial(port, baud, bytesize, timeout=0.1)
 
-    
+        # timeout needs to be >= 0.1 for the controller to be able to answer a call     
+
+
     def init(self):
+
+
+        print "Initializing..."
+
+        self.checkCOM()
+        
+        # initialize x, y, z-axis with saved default parameters 
+        for i in range(1, 4):        
+            self.ser.write("INIT" + str(i) + "\r\n")
+
+        # request current position
+        self.curPos = []
+        for i in range(1, 4):        
+            self.ser.write("?CNT" + str(i) + "\r\n")
+            self.curPos.append(self.ser.readline().replace("\r",""))  
+      
+        print "Current position [x, y, z]: " + str(self.curPos).replace("'","")
+
+
+
 
     # Folgende Parameter muessen beim Initialisieren uebermittelt werden: Motortyp,
     # Endschalter-Maske, Endschalter-Polaritaet, Achsenparameter/Regelparameter, 
     # Strombereich der Motorendstufe. Erst dann kann gefahren werden.
 
-        print "Initializing..."
         
 #        # Motortyp waehlen (3 = Schrittmotor Closed-Loop)
 #        self.ser.write("MOTYPE3\r\n")
 #        # Strombereich für Achse 1/2/3 waehlen (0 = niedrig)        
 #        for i in range(1, 4):        
 #            self.ser.write("AMPSHNT" + str(i) + "=0\r\n")  
-        
-
-        # Achse 1/2/3 initialisieren mit gespeicherten Standardparametern        
-        for i in range(1, 4):        
-            self.ser.write("INIT" + str(i) + "\r\n")
-        for i in range(1, 4):        
-            print "INIT" + str(i) + "\r\n"
-                    
+             
+       
     
     def ref(self):
 
-        # Referenzmaske fuer Achse 1/2/3 waehlen      
+        # choose reference mask (Referenzmaske) for x, y, z      
         for i in range(1, 4):        
             self.ser.write("RMK" + str(i) + "=0001\r\n")
-        # Referenzschalterpolaritaet fuer Achse !/2/3 waehlen
+        # choose reference polarity (Referenzpolaritaet) for x, y, z
         for i in range(1, 4):        
             self.ser.write("RPL" + str(i) + "=0001\r\n")
-         
+        # choose reference mode (Referenzfahrtmodus) for x, y, z
+        for i in range(1, 4):        
+            self.ser.write("REF" + str(i) + "=4\r\n")
+        # status request for x, y, z         
+        self.ser.write("?ASTAT\r\n") 
+
         return True
 
         
-  
 
-    def check(self):
+    def checkCOM(self):
 
         # check if port is open
         if self.ser.isOpen():
             print(self.ser.name + ' is open...')
         else:
             print "Could not find device :("
-
-        return True
-
-
-    def test(self):
-
-        while True:
-            cmd = raw_input("Enter command:")
-            if cmd == "q.":
-                exit()
-            else:
-                self.ser.write(cmd+"\r\n")
-                answer = self.ser.readline()
-                print answer
+            exit()
 
         return True
 
@@ -93,12 +100,76 @@ class owis:
 
         return True
 
+
+    def moveAbs(self, x, y, z):
+       
+        if 0 > x > 1400000 or 0 > y > 1400000 or 0 > z > 100000:
+            print "Out of range!"
+            exit()
+        
+        newPos = [str(x), str(y), str(z)]
+
+        # send new position to controller and start motor movement
+        for i, val in enumerate(newPos):        
+            self.ser.write("PSET" + str(i+1) + "=" + newPos[i] + "\r\n")                
+            self.ser.write("PGO" + str(i+1) + "\r\n")
+        print "Moving to new position..."
+
+        # status request: check current position until goal is reached
+        self.checkPos(newPos, 0.2)
+        
+        for i, val in enumerate(newPos):        
+            self.ser.write("?PSET" + str(i+1) + "\r\n")
+            self.curPos[i] = self.ser.readline().replace("\r","")        
+        print "New position [x, y, z]: " + str(self.curPos).replace("'","")   
+
+        return True
+
+
+    def checkPos(self, pos, wait):
+
+        # read-out temp position until requested position is reached
+        while True:
+            tempPos = []
+            for i, val in enumerate(pos):
+                self.ser.write("?CNT" + str(i+1) + "\r\n")
+                tempPos.append(self.ser.readline().replace("\r",""))
+            if tempPos != pos: 
+                print tempPos 
+                time.sleep(wait)           
+            else:
+                print "Position reached..."                
+                break
+
+        return True
+
+    def Zdrive(self, z):
+        
+        return True    
+    
+
+    def test(self):
+
+        while True:
+            cmd = raw_input("Enter command:")
+            if cmd == "q.":
+                exit()            
+            else:
+                self.ser.write(cmd + "\r\n")
+                answer = self.ser.readline()
+                print answer
+
+        return True        
+
+
 # main loop
 if __name__=='__main__':
 
+
     o = owis()
     o.init()
-    o.check()
     o.test()
+#    o.moveAbs(800000, 800000, 100000)
+
 
 
